64e514e523d8f75633ffdeb53d4386710d2792f0
Your Name <tanjimulislam2027@gmail.com>
2026-02-24 22:27:00 +0600

    files updated

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,4 @@
-// Database configuration constants
-// Database name for MongoDB connection
-export const DB_NAME = "youtube-clone";
-
-// Note: MONGO_URI is now loaded from .env file
-// The full connection string with options should be set in MONGO_URI in .env
-// Example: mongodb+srv://username:password@cluster.mongodb.net/database?options
+export const DB_NAME = 'youtube-clone';
+// export const DB_USER = 'root';
+// export const DB_PASSWORD = 'root';
+// export const DB_HOST = 'localhost'; 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "..constantjs";
-
-const connectDB = async () => {
-    try {
-        const connetionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connectionhost}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED !!", error);
-        process.exit(1);
-    }
-}
+//require("dotenv").config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"; 
+dotenv.config({
+    path: "./.env"
+});
+connectDB();
 
 
-export { connectDB };
+//import mongoose from "mongoose";
+// import { DB_NAME } from "..constantjs";
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`);
+//         // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
+//         // console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log("ERROR:", error);
+//         throw error;
+//     }
+// }
+
+
+// export { connectDB };
